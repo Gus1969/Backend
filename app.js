9b3a862bed5e77a557582cf75e9d3508c72c8096
Gus1969 <gustavo.ubalperez@gmail.com>
2021-11-18 14:21:37 -0300

    changes

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const PORT = 4000;
+const PORT = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({
@@ -14,7 +14,7 @@ app.get('/', (req, res) => {
     res.send('Servidor funcionando');
 });
 
-app.listen(PORT, function() {
+app.listen(PORT,() => {
     console.log(`escuchando en el puerto ${PORT}`);
 
 });
